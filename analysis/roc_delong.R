2eb74a7b897bcdf55d5644e30bf4c494bd7ebaea
Max Kapsecker <max.kapsecker@gmail.com>
2024-12-03 14:38:23 +0000

    ROC: DeLong and CI
    Added R Script for verifying ROC differences using DeLong test and calculating ROC's confidence intervalls.

---
+++ analysis/roc_delong.R
@@ -0,0 +1,27 @@
+require(pROC)
+
+aucs_fed <- read.csv("~/Documents/GitHub/cardioflow/analysis/results/aucs_federated_finetuned.txt")
+aucs_cen <- read.csv("~/Documents/GitHub/cardioflow/analysis/results/aucs_centered.txt")
+
+ftResEmb <- aucs_fed$FTResEmb
+fedResEmb <- aucs_fed$FedResEmb
+cenResEmb <- aucs_cen$CenResEmb
+
+ftRes <- aucs_fed$FTRes
+fedRes <- aucs_fed$FedRes
+cenRes <- aucs_cen$CenRes
+
+label_fed <- aucs_fed$Label
+label_cen <- aucs_cen$Label
+
+roc_ftResEmb <- roc(label_fed, ftResEmb)
+roc_fedResEmb <- roc(label_fed, fedResEmb)
+roc_cenResEmb <- roc(label_cen, cenResEmb)
+
+roc_fedRes <- roc(label_fed, fedRes)
+roc_ftRes <- roc(label_fed, ftRes)
+roc_cenRes <- roc(label_cen, cenRes)
+
+# Choose from above variables which curve(s) to investigate
+deLongResult <- roc.test(roc_fedResEmb, roc_ftRes, method = "delong")
+ci_roc <- ci.auc(roc_ftResEmb, method = "bootstrap", boot.n = 2000, conf.level = 0.95)
